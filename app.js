6fe998304e69fa87e5b3a6af4c512a0f4a89d580
irivinti-gayathri <128351501+irivinti-gayathri@users.noreply.github.com>
2023-09-08 21:40:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const mongoose=require('mongoose');
+// init app
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const {port, engine} = require('./config'); //Es6 Destructuring
+const db = require('./models/db');
+
+// Configuration
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(fileUpload());
+app.use('/public', express.static('public'));
+app.set('view engine', engine);
+
+const controller = require('./libs/controller')(app);
+
+app.listen(port, err => console.info(err ? `Error on port ${port}` : `App running on port ${port}`));
